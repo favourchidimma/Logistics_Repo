a3dffa31f8e6e36b06312e870c27f9ffdb48bc19
favourchidimma <idikafavourchidimma1@gmail.com>
2025-07-25 13:49:54 +0200

    tried chsnging user

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -21,7 +21,7 @@ DEBUG = True  # Set to False for production
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-# Application definition
+# Applicajktion definition
 INSTALLED_APPS = [
     'accounts',
     'django.contrib.admin',
