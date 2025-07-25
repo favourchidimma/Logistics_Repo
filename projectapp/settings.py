32aaebdac89456a785db4d6e53ab1d030cc0fb66
favourchidimma <idikafavourchidimma1@gmail.com>
2025-07-25 14:14:12 +0200

    allowed host after active host

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Set to False for production
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', 'logistics-repo.onrender.com',]
 
 # Application definition
 INSTALLED_APPS = [
